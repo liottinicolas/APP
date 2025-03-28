55d5d69a6f8543af3de451daa6df13003a8b17be
liottinicolas <nicolasliotti92@gmail.com>
2025-03-28 13:31:37 -0300

    Se agrego una función para obtener los datos en un excel por gid busquedagid

--- carga_BD.R
+++ carga_BD.R
@@ -54,9 +54,59 @@ funcion_exportar_incidencias_grua_o_pluma(prueba_pluma,"Pluma")
 
 ############## para probar ####################
 
+ini <- as.Date("2025-03-01")
+fin <- as.Date("2025-03-26")
+prueba_grua_dia <- funcion_mostrar_responsables_por_incidencias(historico_completo_llenado_incidencias,historico_estado_diario,ini,fin,"Grua")
 
 
+  
 
+
+# gid_buscado <- 134160
+funcion_imprimir_datosporgid <-function(gid_buscado){
+  
+  imprimir <- historico_completo_llenado_incidencias %>% 
+    filter(gid == gid_buscado) %>% 
+    select(-Id_Motivo_no_levante,-Accion_requerida,-Responsable,-Circuito,-DB,-Numero_caja)
+  
+  wb <- createWorkbook()
+  
+  # Añadir una hoja
+  addWorksheet(wb, "Datos")
+  
+  # Escribir el data frame como tabla con formato en la hoja "Datos"
+  writeDataTable(wb, sheet = "Datos", x = imprimir, tableStyle = "TableStyleLight9")
+  
+  # Ajustar automáticamente el ancho de todas las columnas (1 a ncol(df_filtrado))
+  setColWidths(wb, sheet = "Datos", cols = 1:ncol(imprimir), widths = "auto")
+  
+  # Crear un estilo para fechas (solo fecha)
+  dateStyle <- createStyle(numFmt = "dd/mm/yyyy")
+  
+  # Suponiendo que la columna Fecha es la segunda columna, se aplica estilo a todas las celdas de esa columna
+  addStyle(wb, sheet = "Datos", style = dateStyle, 
+           cols = 2, rows = 2:(nrow(imprimir) + 1), gridExpand = TRUE)
+  
+  nombre_archivo <- paste0("datos_gid_", gid_buscado, ".xlsx")
+
+  
+  saveWorkbook(wb, file = nombre_archivo, overwrite = TRUE)
+  
+}  
+
+funcion_obtener_datosporgid <-function(gid_buscado){
+  
+  imprimir <- historico_completo_llenado_incidencias %>% 
+    filter(gid == gid_buscado) %>% 
+    select(-Id_Motivo_no_levante,-Accion_requerida,-Responsable,-Circuito,-DB,-Numero_caja)
+  
+return(imprimir)
+  
+}  
+
+funcion_imprimir_datosporgid(134160)
+asd_177075 <- funcion_obtener_datosporgid(177075)
+asd_181129 <- funcion_obtener_datosporgid(181129)
 
 
 
