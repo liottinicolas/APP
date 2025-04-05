173e85162a0b4c4ce926ac113e7cc6a1bc918249
liottinicolas <nicolasliotti92@gmail.com>
2025-04-05 13:41:49 -0300

    Le agregue a los circuitos FRECUENCIA Y PERIODO

--- scripts/para_mapear/circuitos_planificados.R
+++ scripts/para_mapear/circuitos_planificados.R
@@ -1,4 +1,3 @@
-
 ## Cargar los datos
 
 
@@ -92,26 +91,65 @@ turnos_planificados_por_circuito <- function(){
   df_final <- df_final %>% 
     select(cod_recorrido,circuito_corto,municipio,numero_circuito,turno_planificado)
   
-  # Agrega columna de Frecuencia (valor por defecto)
-  df_final$Frecuencia <- 7
+  # Crear mapeo de frecuencias específicas para cada circuito
+  frecuencias_por_circuito <- list(
+    "A_103" = 3.50, "A_104" = 3.50, "A_105" = 3.50, "A_106" = 3.50,
+    "A_107" = 3.00, "A_108" = 3.00, "A_109" = 3.00, "A_110" = 3.00,
+    "A_111" = 3.00, "A_112" = 3.00, "A_113" = 3.00, "A_114" = 3.00,
+    "A_116" = 3.00, "A_119" = 3.00, "A_201" = 3.00, "A_202" = 3.00,
+    "A_203" = 3.00, "A_204" = 3.00, "A_205" = 3.00, "A_206" = 3.00,
+    "B_101" = 3.00, "B_102" = 3.00, "B_103" = 7.00,
+    "C_101" = 3.00, "C_102" = 3.00, "C_103" = 3.00, "C_104" = 3.00,
+    "C_105" = 3.00, "C_106" = 3.00, "C_107" = 3.00, "C_108" = 3.00,
+    "C_109" = 3.00, "C_110" = 3.00, "C_111" = 3.00, "C_112" = 3.00,
+    "C_113" = 3.00, "C_114" = 3.00, "C_115" = 3.00, "C_116" = 3.00,
+    "CH_101" = 7.00, "CH_102" = 7.00, "CH_103" = 7.00, "CH_104" = 7.00,
+    "CH_105" = 7.00, "CH_106" = 7.00, "CH_107" = 7.00, "CH_108" = 7.00,
+    "CH_109" = 7.00, "CH_110" = 3.50, "CH_111" = 3.50, "CH_112" = 3.50,
+    "CH_113" = 3.50, "CH_114" = 3.50, "CH_115" = 3.50, "CH_116" = 3.50,
+    "CH_117" = 3.50, "CH_118" = 3.50, "CH_119" = 3.50, "CH_120" = 3.50,
+    "CH_121" = 3.50, "CH_122" = 7.00, "CH_123" = 7.00,
+    "D_101" = 3.50, "D_102" = 3.50, "D_103" = 3.50, "D_104" = 3.50,
+    "D_105" = 3.50, "D_106" = 3.50, "D_107" = 3.50, "D_108" = 3.50,
+    "D_109" = 3.50, "D_110" = 3.50, "D_111" = 3.50, "D_112" = 3.50,
+    "D_113" = 3.50, "D_114" = 3.00, "D_115" = 3.00, "D_116" = 3.00,
+    "D_117" = 3.00, "D_118" = 3.00, "D_119" = 3.00, "D_120" = 3.00,
+    "D_121" = 3.00,
+    "E_101" = 3.00, "E_102" = 3.00, "E_103" = 3.00, "E_104" = 3.00,
+    "E_105" = 3.00, "E_106" = 3.00, "E_107" = 3.00, "E_108" = 3.00,
+    "E_109" = 2.33, "E_110" = 3.00, "E_111" = 3.00, "E_112" = 2.33,
+    "E_113" = 2.33, "E_114" = 2.33, "E_115" = 2.33, "E_116" = 2.33,
+    "E_136" = 7.00,
+    "F_101" = 3.00, "F_102" = 3.00, "F_103" = 3.00, "F_104" = 3.00,
+    "F_105" = 3.00, "F_106" = 3.00, "F_107" = 3.00, "F_108" = 3.00,
+    "F_109" = 3.00, "F_110" = 3.00, "F_111" = 3.00, "F_112" = 3.00,
+    "F_113" = 3.00, "F_114" = 3.00, "F_115" = 3.00,
+    "G_101" = 3.00, "G_103" = 3.00, "G_104" = 3.00, "G_105" = 3.00,
+    "G_106" = 3.00, "G_107" = 3.00, "G_108" = 3.00, "G_109" = 3.00,
+    "G_110" = 3.00, "G_111" = 3.00, "G_112" = 3.00, "G_113" = 3.00,
+    "G_114" = 3.00, "G_115" = 3.00, "G_116" = 3.00
+  )
   
-  # Modificar algunos valores de frecuencia según reglas específicas (estos son ejemplos)
-  df_final <- df_final %>%
-    mutate(Frecuencia = case_when(
-      municipio == "A" & turno_planificado == "N" ~ "Semanal",
-      municipio == "CH" ~ "Interdiaria",
-      TRUE ~ Frecuencia
-    ))
+  # Asignar frecuencias específicas a cada circuito
+  df_final$Frecuencia <- sapply(df_final$circuito_corto, function(circuito) {
+    if (circuito %in% names(frecuencias_por_circuito)) {
+      as.character(frecuencias_por_circuito[[circuito]])
+    } else {
+      "7" # Valor predeterminado si no está en la lista
+    }
+  })
+  
+  # Calcular la columna "Periodo" como 7/Frecuencia
+  df_final$Periodo <- round(7 / as.numeric(df_final$Frecuencia), 2)
   
   df_final$Fecha_inicio <- as.Date("2024-10-10")
   df_final$FechaFin <- as.Date(NA)
-  
-  
   
   return(df_final)
 }
 
 # Función para registrar cambios históricos
+# Si se actualiza la Frecuencia, también se recalcula el Periodo
 actualizar_circuito <- function(df_actual, cod_recorrido, nuevos_valores, fecha_cambio = Sys.Date()) {
   # Encuentra el circuito actual y marca su fecha de fin
   idx <- which(df_actual$cod_recorrido == cod_recorrido & is.na(df_actual$FechaFin))
@@ -125,6 +163,12 @@ actualizar_circuito <- function(df_actual, cod_recorrido, nuevos_valores, fecha_
     for(col in names(nuevos_valores)) {
       nueva_fila[[col]] <- nuevos_valores[[col]]
     }
+    
+    # Si se actualizó la frecuencia, recalcular el periodo
+    if("Frecuencia" %in% names(nuevos_valores)) {
+      nueva_fila$Periodo <- round(7 / as.numeric(nueva_fila$Frecuencia), 2)
+    }
+    
     nueva_fila$Fecha_inicio <- fecha_cambio
     nueva_fila$FechaFin <- as.Date(NA)
     
@@ -141,5 +185,5 @@ prueba <- turnos_planificados_por_circuito()
 # Cambiar turno y frecuencia del circuito A_101
 prueba <- actualizar_circuito(prueba, 
                             "A_DU_RM_CL_101", 
-                            list(turno_planificado = "V", Frecuencia = "Semanal"),
+                            list(turno_planificado = "V", Frecuencia = "3.5"),
                             as.Date("2024-11-01"))
